6f296a949b5c0f38bcb76662c21706fb0156788c
Kadyr <kadyr.m2002@gmail.com>
2024-02-08 16:10:08 +0600

    SwiftLint и Swiftformat

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  UIKitDZ
-//
-//  Created by Қадыр Маратұлы on 08.02.2024.
-//
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
 
+/// ViewController
 class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+//        // Do any additional setup after loading the view.
+//    }
 }
-
